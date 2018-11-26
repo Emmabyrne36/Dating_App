1f5bc8fa3186d6228a86b93c1d87c6982f25f82b
Emmabyrne36 <Emmabyrne36@gmail.com>
2018-11-26 12:51:40 +0000

    Policy based authorisation

--- Identity/DatingApp.API/Startup.cs
+++ Identity/DatingApp.API/Startup.cs
@@ -74,6 +74,13 @@ namespace DatingApp.API
                    };
                });
 
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+                options.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+                options.AddPolicy("VipOnly", policy => policy.RequireRole("VIP"));
+            });
+
             services.AddMvc(options =>
             {
                 var policy = new AuthorizationPolicyBuilder()
