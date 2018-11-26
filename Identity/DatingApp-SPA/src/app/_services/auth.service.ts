78b49500c02782dd60a248362242d3897a29f1f5
Emmabyrne36 <Emmabyrne36@gmail.com>
2018-11-26 15:22:43 +0000

    Updated auth guard to protect routes based on user role

--- Identity/DatingApp-SPA/src/app/_services/auth.service.ts
+++ Identity/DatingApp-SPA/src/app/_services/auth.service.ts
@@ -47,4 +47,17 @@ export class AuthService {
     const token = localStorage.getItem('token');
     return !this.jwtHelper.isTokenExpired(token); // if token is not expired: true, if token is expired: false
   }
+
+  roleMatch(allowedRoles): boolean {
+    let isMatch = false;
+    const userRoles = this.decodedToken.role as Array<string>;
+    allowedRoles.forEach(element => {
+      if (userRoles.includes(element)) {
+        isMatch = true;
+        return;
+      }
+    });
+
+    return isMatch;
+  }
 }
