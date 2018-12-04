ae8737f58d633ef8cbf7d8389f184ee02fadcbf1
Emmabyrne36 <Emmabyrne36@gmail.com>
2018-12-04 11:59:13 +0000

    Added the modal component. Passed roles data into the modal

--- Identity/DatingApp-SPA/src/app/admin/user-management/user-management.component.ts
+++ Identity/DatingApp-SPA/src/app/admin/user-management/user-management.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/_models/user';
 import { AdminService } from 'src/app/_services/admin.service';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap';
+import { RolesModalComponent } from '../roles-modal/roles-modal.component';
 
 @Component({
   selector: 'app-user-management',
@@ -9,8 +11,9 @@ import { AdminService } from 'src/app/_services/admin.service';
 })
 export class UserManagementComponent implements OnInit {
   users: User[];
+  bsModalRef: BsModalRef;
 
-  constructor(private adminService: AdminService) { }
+  constructor(private adminService: AdminService, private modalService: BsModalService) { }
 
   ngOnInit() {
     this.getUsersWithRoles();
@@ -22,6 +25,44 @@ export class UserManagementComponent implements OnInit {
     }, error => {
       console.log(error);
     });
+  }
+
+  editRolesModal(user: User) {
+    const initialState = {
+      user,
+      roles: this.getRolesArray(user),
+      title: 'Modal with component'
+    };
+    this.bsModalRef = this.modalService.show(RolesModalComponent, {initialState});
+    this.bsModalRef.content.closeBtnName = 'Close';
+  }
+
+  private getRolesArray(user: User) {
+    const roles = [];
+    const userRoles = user.roles;
+    const availableRoles: any[] = [
+      {name: 'Admin', value: 'Admin'},
+      {name: 'Moderator', value: 'Moderator'},
+      {name: 'Member', value: 'Member'},
+      {name: 'VIP', value: 'VIP'}
+    ];
+    // Check if available roles match the userRoles
+    for (let i = 0; i < availableRoles.length; i++) {
+      let isMatch = false;
+      for (let j = 0; j < userRoles.length; j++) {
+        if (availableRoles[i].name === userRoles[j]) {
+          isMatch = true;
+          availableRoles[i].checked = true;
+          roles.push(availableRoles[i]);
+          break;
+        }
+      }
+      if (!isMatch) {
+        availableRoles[i].checked = false;
+        roles.push(availableRoles[i]);
+      }
+    }
+    return roles;
   }
 
 }
